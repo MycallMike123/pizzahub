f74f6a7c4ee459d77a25ce8c2d678f411c25d1bb
Michael Mwangi <michaelmichire2016.com>
2024-05-28 06:39:10 +0000

    Update

--- pizzaflask/__init__.py
+++ pizzaflask/__init__.py
@@ -1,25 +1,18 @@
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
-
 from flask_migrate import Migrate
-
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pizza.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-
 from flaskpizza import routes
-# Pass the required route to the decorator.
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return {'message': "Hello, Welcome to test my api"
-        }
-
-
+    return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(debug=True, port = 5555)
+    app.run(debug=True, port=5555)
